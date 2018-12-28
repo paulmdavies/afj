d52461095d4314be276520df266a1f546a0a4f00
Paul Davies <paulmdavies@gmail.com>
2018-12-28 09:43:21 +0000

    Add toy server.yml and config to match

--- src/main/java/com/afjtravel/website/ServiceConfiguration.java
+++ src/main/java/com/afjtravel/website/ServiceConfiguration.java
@@ -1,6 +1,20 @@
 package com.afjtravel.website;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class ServiceConfiguration extends Configuration {
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
 }
